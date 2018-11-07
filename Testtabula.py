186c0401d54e5df3e62aee35a97a5d6af9bba545
AlcyonSuisse <romain.bika@gmail.com>
2018-11-07 21:11:45 +0100

    Voir pour finir en ajoutant le contenue de test.py => execution de la macro excel par python

--- Testtabula.py
+++ Testtabula.py
@@ -1,53 +1,77 @@
 # -*- coding: utf-8 -*-
+import sys
+import os
 import tabula
 import pandas as pd
-from openpyxl import load_workbook
-from openpyxl.utils.dataframe import dataframe_to_rows
+import Tkinter as tk
+import tkMessageBox
+from tkFileDialog import askopenfilename
+#from openpyxl import load_workbook
+#from openpyxl.utils.dataframe import dataframe_to_rows
 from datetime import date
 #TODO : Choix du fichier à lire
 
-# Read pdf into DataFrame
-df = tabula.read_pdf("test.pdf", pages='all', multiple_tables=True)
-#on supprime les 3 premieres lignes
-df = [df[i].iloc[3:,] for i in range(len(df))]
+#Fenetre tres simple pour choisir le fichier PDF
+def choixfichier():
+    root = tk.Tk()
+    root.withdraw()                 # pour ne pas afficher la fenêtre Tk
+    name = askopenfilename()   # lance la fenêtre
+    print(name)
+    _, ext = os.path.splitext(name)
+    print(ext)
 
-#On merge les tableaux de df
-merge = pd.DataFrame()
-merge = pd.concat([i for i in df],ignore_index=True)
-
-# on va essayer de filtrer les identifiants des échantillons, soit bon, soit coulée soit fils de la ligne précédente
-for row in merge.itertuples():
-    print(row)
-    #print(merge.iloc[row[0],3])
-    #Un casier a maximum 4 chiffre
-    if len(row[4]) > 4:
-        merge.iloc[row[0],3] = merge.iloc[row[0],3].replace(" ", "-")
+    if ext == ".pdf":
+        return name
     else:
-        merge.iloc[row[0], 3] = merge.iloc[row[0]-1, 3]
+        return False
 
-#TODO : Si la date n'est pas en colonne 9(10 dans le namedtuple), la copier de la colonne 10(11 dans le namedtuple), puis sup la colonne 10
-    print(type(row[10]))
-    if type(row[10]) is float:
-        merge.iloc[row[0], 9] = merge.iloc[row[0], 10]
 
-#On supprime la colonne 10 et 4
-merge = merge.drop([4, 10], axis=1)
-print(merge)
+def extrairepdf(Cheminpdf):
+    # Read pdf into DataFrame
+    print(Cheminpdf)
+    df = tabula.read_pdf(Cheminpdf, pages='all', multiple_tables=True)
+    #on supprime les 3 premieres lignes
+    df = [df[i].iloc[3:,] for i in range(len(df))]
 
-#Ajout d'une feuille et copie des valeurs
-# wb = load_workbook(filename='merge.xlsx')
-# #TODO : Changer le nom de l'onglet avec la date
-# ws = wb.create_sheet(date.today().strftime("%d-%m-%y"))
-#
-# for r in dataframe_to_rows(merge, index=False, header=False):
-#     ws.append(r)
-#
-# wb.save('merge.xlsx')
-HeaderExcel = ["Type","Famille","Nuance","Identifiant échantillon","PE","Réacteur","Fiole","Observations","Délai"]
+    #On merge les tableaux de df
+    #merge = pd.DataFrame()
+    merge = pd.concat([i for i in df],ignore_index=True)
 
-writer = pd.ExcelWriter('merge.xlsx', )
-merge.to_excel(writer, date.today().strftime("%d-%m-%y"), header=HeaderExcel, index=False)
-writer.save()
+    # on va essayer de filtrer les identifiants des échantillons, soit bon, soit coulée soit fils de la ligne précédente
+    for row in merge.itertuples():
+        #print(row)
+        #print(merge.iloc[row[0],3])
+        #Un casier a maximum 4 chiffre
+        if len(row[4]) > 4:
+            merge.iloc[row[0],3] = merge.iloc[row[0],3].replace(" ", "-")
+        else:
+            merge.iloc[row[0], 3] = merge.iloc[row[0]-1, 3]
+
+    #TODO : Si la date n'est pas en colonne 9(10 dans le namedtuple), la copier de la colonne 10(11 dans le namedtuple), puis sup la colonne 10
+        #print(type(row[10]))
+        if type(row[10]) is float:
+            merge.iloc[row[0], 9] = merge.iloc[row[0], 10]
+
+    #On supprime la colonne 10 et 4
+    merge = merge.drop([4, 10], axis=1)
+    print(merge)
+    return merge
+
+def creerexcel(valeur):
+    HeaderExcel = ["Type","Famille","Nuance","Identifiant échantillon","PE","Réacteur","Fiole","Observations","Délai"]
+
+    writer = pd.ExcelWriter('merge.xlsx', )
+    valeur.to_excel(writer, date.today().strftime("%d-%m-%y"), header=HeaderExcel, index=False)
+    writer.save()
+
+
+if __name__ == '__main__':
+    Nonfichier = choixfichier()
+    if Nonfichier is False:
+        tkMessageBox.showerror("Titre", "Mauvais fichier! ce n'est pas un pdf")
+        sys.exit()
+    else:
+        creerexcel(extrairepdf(Nonfichier))
 
 
 
--- test.py
+++ test.py
@@ -1,2 +1,15 @@
-from datetime import date
-import xlsxwriter
+# -*- coding: utf-8 -*-
+import os, os.path
+import win32com.client
+
+try:
+    xlApp = win32com.client.DispatchEx('Excel.Application')
+    xlsPath = os.path.expanduser('C:\Users\Romain\PycharmProjects\pdftoexcel\Test.xlsm')
+    wb = xlApp.Workbooks.Open(Filename=xlsPath)
+    xlApp.Run('ImportFeuille')
+    wb.Save()
+    xlApp.Quit()
+    print("Macro ran successfully!")
+except:
+    print("Error found while running the excel macro!")
+    xlApp.Quit()
